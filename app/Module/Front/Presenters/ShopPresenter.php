56fdb564f20d93e5b051daa4462b471ddf5911a2
pohrebacek <petrjan872@gmail.com>
2025-05-22 21:50:20 +0200

    c

---
+++ app/Module/Front/Presenters/ShopPresenter.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Module\Front\Presenters;
+
+use Nette;
+
+final class ShopPresenter extends BasePresenter {
+    public function renderCart(): void {
+
+    }
+
+    public function handleSubmitCart(): void {
+
+    }
+
+    public function kkt() {
+        return "kok";
+    }
+}
