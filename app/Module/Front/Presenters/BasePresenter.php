5fda0cb4010eb12153ef4c8515d580aed5ef97fe
pohrebacek <petrjan872@gmail.com>
2025-07-18 12:07:26 +0200

    práce na šipečce back

--- app/Module/Front/Presenters/BasePresenter.php
+++ app/Module/Front/Presenters/BasePresenter.php
@@ -14,12 +14,23 @@ class BasePresenter extends Presenter
         $acl = MyAuthorizator::create();
 		$this->getUser()->setAuthorizator($acl);
 
-        $session = $this->getSession('navigation');
-        $current = $this->getHttpRequest()->getUrl()->getAbsoluteUrl();
-        $previous = $session->last ?? null;
-        
-        $session->previous = $current;
-        $this->template->lastUrl = $previous;
+        //$session = $this->getSession('navigation');
+        //$current = $this->getHttpRequest()->getUrl()->getAbsoluteUrl();
+        //$previous = $session->last ?? null;
+        //
+        //$session->previous = $current;
+        //$this->template->lastUrl = $previous;
+
+        $session = $this->getSession()->getSection('navigation');
+        $currentUrl = $this->getHttpRequest()->getUrl()->getAbsoluteUrl();
+
+        // uložíme předchozí URL jen pokud je aktuální URL jiná než ta uložená
+        if (isset($session->currentUrl) && $session->currentUrl !== $currentUrl) {
+            $session->previousUrl = $session->currentUrl;
+        }
+
+        $session->currentUrl = $currentUrl;
+        bdump($currentUrl);
     }
 
 }
